37083c1d21edc877e04ef2613a11e49669e1afca
Khadijah Coakley <khadijah@wildfyregroup.com>
2016-07-18 20:54:51 -0500

    gz file removed

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140828012915) do
+ActiveRecord::Schema.define(version: 20160719015303) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "case_studies", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "placement_event_id"
+    t.string   "project_name"
+    t.text     "project_desc"
+    t.integer  "project_type_id"
+    t.string   "project_type_other"
+    t.string   "country"
+    t.string   "region"
+    t.string   "county"
+    t.string   "city"
+    t.integer  "district_id"
+    t.integer  "saga_id"
+    t.integer  "survey_status_id"
+    t.date     "survey_create_date"
+    t.date     "survey_update_date"
+    t.integer  "project_area"
+    t.integer  "project_area_uom_id"
+    t.string   "case_study_pk"
+    t.string   "integer"
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
