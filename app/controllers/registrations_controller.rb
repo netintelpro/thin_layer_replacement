97e470d6ef902263f0de58deff858701ca177ef8
Khadijah Coakley <khadijah@wildfyregroup.com>
2016-07-18 19:29:34 -0500

    gif removed, registrations controller added

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,29 @@
+class RegistrationsController < Devise::RegistrationsController
+  layout "minimal"
+  layout 'application', only: [:edit] 
+
+ 	private
+
+ 	def sign_up_params
+   		params.require(:user).permit(:name, :email,:password, :password_confirmation, )
+ 	end
+
+ 	def account_update_params
+   		params.require(:user).permit(: :name, :email,:password, :password_confirmation, :current_password)
+ 	end
+
+ 	def edit
+   		render layout: "application"
+ 	end
+
+ 	#protected
+	#def after_sign_up_path_for(resource)
+		#after_signup_path(:location)
+	#end
+
+	#def after_update_path_for(resource)
+		#after_signup_path(:location)
+	#end
+	
+end
+
