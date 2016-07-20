0f6d2017330ea3b48851f8f3670c83e6cc616bb0
Khadijah Coakley <khadijah@wildfyregroup.com>
2016-07-19 19:38:49 -0500

    updates to case study pages, asset debugging

--- config/initializers/devise_permitted_parameters.rb
+++ config/initializers/devise_permitted_parameters.rb
@@ -1,7 +1,15 @@
-#module DevisePermittedParameters
- # extend ActiveSupport::Concern
+module DevisePermittedParameters
+  extend ActiveSupport::Concern
 
-  
-#end
+  included do
+    before_action :configure_permitted_parameters
+  end
 
+  protected
 
+  def configure_permitted_parameters
+  	devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email,   :password, :password_confirmation, :current_password, :firstname, :middlename, :lastname, :nickname) }
+  end
+end
+
+DeviseController.send :include, DevisePermittedParameters
