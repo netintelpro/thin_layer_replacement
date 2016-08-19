f585bcaded4291e7ce6fae52c094025c8125637a
Abdul Barek <barek2k2@gmail.com>
2016-08-19 23:15:02 +0600

    pg used instead of sqlite

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160819135628) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "case_studies", force: :cascade do |t|
     t.integer  "user_id"
     t.string   "placement_event_id"
@@ -63,7 +66,7 @@ ActiveRecord::Schema.define(version: 20160819135628) do
     t.integer  "role"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
