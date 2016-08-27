bb434400f416cd8c0f1fced2e919b7f8d9a11769
Khadijah Coakley <khadijah@wildfyregroup.com>
2016-08-27 18:45:52 -0500

    updates to the sign-up pages

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160827140606) do
+ActiveRecord::Schema.define(version: 20160827165922) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20160827140606) do
     t.string   "author_ids"
     t.string   "latitude"
     t.string   "longitude"
+    t.text     "measurements"
   end
 
   create_table "documents", force: :cascade do |t|
